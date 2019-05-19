5eed39b47885f222d8e8707a7e0cb4e3f0dc7d61
Alex Jin <alexjinchina@hotmail.com>
2019-05-19 14:24:38 +0800

    add react-native-path

--- GameTools/src/utils.js
+++ GameTools/src/utils.js
@@ -3,26 +3,26 @@ import { PermissionsAndroid } from "react-native";
 import RNFS from "react-native-fs";
 import lodash from "lodash";
 
-// import path from "path";
-// export { path };
+import RNPath from "react-native-path";
 
 import { name as APP_NAME, displayName as APP_DISPLAY_NAME } from "../app.json";
 export { APP_NAME, APP_DISPLAY_NAME, lodash };
 
 export const fs = RNFS;
 
-// export const path = {
-//   append(base, ...comps) {
-//     comps.forEach(comp => {
-//       if (base[base.length - 1] !== "/") {
-//         base += "/";
-//       }
-//       base += comp;
-//     });
+export const path = {
+  ...RNPath,
+  join(base, paths) {
+    let out = base;
+    paths.forEach(p => {
+      if (!out.endsWith("/")) out += "/";
 
-//     return base;
-//   }
-// };
+      out += p;
+    });
+
+    return out;
+  }
+};
 
 export const Permission = {
   READ_EXTERNAL_STORAGE: PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
